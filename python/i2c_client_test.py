b31b514638fea9326246021ee514c8c98a07b44c
Pat Deegan <pat.devel@psychogenic.com>
2025-04-29 16:35:04 -0400

    docs for sat simulator

--- python/i2c_client_test.py
+++ python/i2c_client_test.py
@@ -11,19 +11,34 @@ SlaveAddress = 0x51
 ResponseDelaySeconds = 0.5
 
 class SatelliteSimulator:
-    
+    '''
+        Talks to spasics satellite board over I2C
+        and knows how to craft message and interpret 
+        data coming back.
+    '''
     def __init__(self, scl:int=25, sda:int=24, baudrate:int=100000):
         self._i2c = machine.I2C(0, scl=25, sda=24, freq=100000)
         self._start_time = time.time()
         self._ping_count = 0
         
     def send(self, bts:bytearray):
+        '''
+            send raw bytes over to device
+        '''
         self._i2c.writeto(SlaveAddress, bts)
         
     def read_block(self):
+        '''
+            read a block of 16 bytes from device
+        '''
         return self._i2c.readfrom(SlaveAddress, 16)
     
     def read_pending(self):
+        '''
+            read blocks of 16 bytes until you hit 
+            and "empty" response (all 0xff)
+            and interpret the data
+        '''
         empty = bytearray([0xff]*16)
         
         rcvd = []
@@ -38,19 +53,22 @@ class SatelliteSimulator:
                     return rcvd[0] 
                 return rcvd
             
-            # print(f"Got data block {blk}")
+            
             elif blk[0] == 0x01:
-                # system message
+                # system message: 0x01 ...
                 if blk[1]:
-                    # ok message
+                    # ok message: 0x01 [NON-ZERO] ...
                     if blk[1] == 0x01:
+                        # 0x01 0x01 : OK
                         return 'OK'
                     if blk[1] == 0x02:
+                        # 0x01 0x2 LEN MSG[LEN]: OK WITH PAYLOAD
                         msglen = blk[4]
                         msg = blk[5:(5+msglen)]
                         rcvd.append( f'OK: {msg}' )
                 else:
                     # error message
+                    # 0x01 0x00 ERRCODE LEN MSG[LEN]
                     errcode = blk[2]
                     errlen = blk[3]
                     if errlen:
@@ -60,6 +78,7 @@ class SatelliteSimulator:
                     rcvd.append( f'ERROR [{errcode}] {errmsg}' )
             
             elif blk[0] == 0x07:
+                # 0x07 RUNNING EXPERIMENTID EXCEPTIONID RUNTIME[4] RESULT[8]
                 running = True if blk[1] else False
                 expid = blk[2]
                 exception_id = blk[3]
@@ -73,7 +92,7 @@ class SatelliteSimulator:
             
                 
             elif blk[0] == 0x09:
-                # 0x09 EXPERIMENTID COMPLETED EXCEPT_ID LEN RESULTBYTES (number of bytes depends on experiment)
+                # 0x09 EXPERIMENTID COMPLETED EXCEPT_ID LEN RESULTBYTES[LEN]
                 expid = blk[1]
                 completed = blk[2]
                 except_id = blk[3]
@@ -99,6 +118,7 @@ class SatelliteSimulator:
         
     
     def abort(self):
+        # b'A'
         print("Requesting experiment abort")
         bts = bytearray([ord('A')])
         self.send(bts)
@@ -107,6 +127,7 @@ class SatelliteSimulator:
         
         
     def time_sync(self):
+        # 'T' and 4 bytes of time
         t_now = time.time() - self._start_time
         bts = bytearray([ord('T')])
         bts += t_now.to_bytes(4, 'little')
@@ -114,6 +135,7 @@ class SatelliteSimulator:
         self.send(bts)
         
     def status(self):
+        # b'S'
         print("Requesting status")
         bts = bytearray([ord('S')])
         self.send(bts)
@@ -121,6 +143,7 @@ class SatelliteSimulator:
         print(f'Response: {self.read_pending()}')
         
     def ping(self):
+        # 'P' and payload byte to get back in pong
         self._ping_count += 1
         print(f"Sending ping {self._ping_count}")
         bts = bytearray([ord('P'), self._ping_count % 256])
@@ -129,6 +152,7 @@ class SatelliteSimulator:
         print(f'Response: {self.read_pending()}')
         
     def reboot(self, safe_mode:bool=False):
+        # 'R' and one byte (safemode, unimplemented)
         print(f"Sending reboot command")
         bts = bytearray([ord('R'), 1 if safe_mode else 0])
         self.send(bts)
@@ -136,6 +160,7 @@ class SatelliteSimulator:
         print(f'Response: {self.read_pending()}')
         
     def run_experiment_now(self, experiment_id:int, args:bytearray=None):
+        # 'E' EXPID[2]
         print(f"Requesting run of experiment {experiment_id}")
         bts = bytearray([ord('E')])
         bts += experiment_id.to_bytes(2, 'little')
