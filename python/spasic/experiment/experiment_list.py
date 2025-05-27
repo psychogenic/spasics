1dad0ae2421c648f38602c7dc832b2468e64c643
Pat Deegan <pat.devel@psychogenic.com>
2025-05-27 19:54:21 -0400

    Experiment list reworked with Troy's cool (i.e. working) importer

--- python/spasic/experiment/experiment_list.py
+++ python/spasic/experiment/experiment_list.py
@@ -4,73 +4,59 @@
 '''
 import gc 
 
-ExperimentsAvailable = {
+DefaultFuncName = 'run_experiment'
+ModuleParent = 'spasic.experiment'
+ExperimentsConfig = {
+    1: ('tt_um_test.loader', None),
+    2: ('tt_um_fstolzcode.loader', None),
+    3: ('tt_um_oscillating_bones.loader', None),
+    4: ('tt_um_qubitbytes_alive.loader', None),
+    5: ('tt_um_urish_spell.loader', None),
+    6: ('wokwi_universal_gates_049.loader', None),
+    7: ('tt_um_andrewtron3000.loader', None),
+    8: ('tt_um_MichaelBell_tinyQV.loader', None),
+    9: ('tt_um_lisa.loader', None),
+    10: ('tt_um_ttrpg_dice.loader', None),
+    11: ('tt_um_msg_in_a_bottle.loader', None),
+    12: ('tt_um_cejmu.loader', None),
+    
+    
+    
+    
+    0x80: ('tt_contributors.loader', 'thank_contribs'),
+    0x81: ('tt_test_experiment.failer', None),
+    0x82: ('tt_um_factory_test.loader', None),
+    0x83: ('tt_test_experiment.forever', None)
 }
-
+ExperimentsAvailable = {}
 def getExperiment(eid:int):
+    
     if eid in ExperimentsAvailable:
         return ExperimentsAvailable[eid]
     
+    if eid not in ExperimentsConfig:
+        return None 
     
     def_thresh = gc.threshold()
     gc.threshold(4096)
-    if eid == 1:
-        import spasic.experiment.tt_um_test.loader
-        ExperimentsAvailable[eid] = spasic.experiment.tt_um_test.loader.run_experiment
-    elif eid == 2:
-        import spasic.experiment.tt_um_fstolzcode.loader
-        ExperimentsAvailable[eid] = spasic.experiment.tt_um_fstolzcode.loader.run_experiment
-    elif eid == 3:
-        import spasic.experiment.tt_um_oscillating_bones.loader
-        ExperimentsAvailable[eid] = spasic.experiment.tt_um_oscillating_bones.loader.run_experiment
-    elif eid == 4:
-        import spasic.experiment.tt_um_qubitbytes_alive.loader
-        ExperimentsAvailable[eid] = spasic.experiment.tt_um_qubitbytes_alive.loader.run_experiment
     
-    elif eid == 5:
-        import spasic.experiment.tt_um_urish_spell.loader
-        ExperimentsAvailable[eid] = spasic.experiment.tt_um_urish_spell.loader.run_experiment
-    elif eid == 6:
-        import spasic.experiment.wokwi_universal_gates_049.loader
-        ExperimentsAvailable[eid] = spasic.experiment.wokwi_universal_gates_049.loader.run_experiment
     
-    elif eid == 7:
-        import spasic.experiment.tt_um_andrewtron3000.loader
-        ExperimentsAvailable[eid] = spasic.experiment.tt_um_andrewtron3000.loader.run_experiment
+    module_path = f'{ModuleParent}.{ExperimentsConfig[eid][0]}'
+    try:
+        # Troy's cool import code
+        mod = __import__(module_path, None, None, ['']) # [''] seems to impact absolute/relative lookup
+        entry_point = ExperimentsConfig[eid][1] if ExperimentsConfig[eid][1] is not None else  DefaultFuncName
         
+        gc.threshold(def_thresh)
         
-    elif eid == 8:
-        import spasic.experiment.tt_um_MichaelBell_tinyQV.loader
-        ExperimentsAvailable[eid] = spasic.experiment.tt_um_MichaelBell_tinyQV.loader.run_experiment
-    
-    elif eid == 9:
-        import spasic.experiment.tt_um_lisa.loader
-        ExperimentsAvailable[eid] = spasic.experiment.tt_um_lisa.loader.run_experiment
-    elif eid == 10:
-        import spasic.experiment.tt_um_ttrpg_dice.loader
-        ExperimentsAvailable[eid] = spasic.experiment.tt_um_ttrpg_dice.loader.run_experiment
-    elif eid == 11:
-        import spasic.experiment.tt_um_msg_in_a_bottle.loader
-        ExperimentsAvailable[eid] = spasic.experiment.tt_um_msg_in_a_bottle.loader.run_experiment
-    elif eid == 12:
-        import spasic.experiment.tt_um_cejmu.loader
-        ExperimentsAvailable[eid] = spasic.experiment.tt_um_cejmu.loader.run_experiment
-    elif eid == 0x80:
-        import spasic.experiment.tt_contributors.loader
-        ExperimentsAvailable[eid] = spasic.experiment.tt_contributors.loader.thank_contribs
-    elif eid == 0x81:
-        import spasic.experiment.tt_test_experiment.failer
-        ExperimentsAvailable[eid] = spasic.experiment.tt_test_experiment.failer.run_experiment
-    elif eid == 0x82:
-        import spasic.experiment.tt_um_factory_test.loader
-        ExperimentsAvailable[eid] = spasic.experiment.tt_um_factory_test.loader.run_experiment
-    elif eid == 0x83:
-        import spasic.experiment.tt_test_experiment.forever
-        ExperimentsAvailable[eid] = spasic.experiment.tt_test_experiment.forever.run_experiment
-    
-    if eid in ExperimentsAvailable:
-        return ExperimentsAvailable[eid]
-    
-    gc.threshold(def_thresh)
-    return None
+        loader = getattr(mod, entry_point, None)
+        if callable(loader):
+            ExperimentsAvailable[eid] = loader
+        else:
+            # something is wrong here, don't do this again
+            ExperimentsAvailable[eid] = None
+    except:
+        ExperimentsAvailable[eid] = None
+        
+    return ExperimentsAvailable[eid]
 
